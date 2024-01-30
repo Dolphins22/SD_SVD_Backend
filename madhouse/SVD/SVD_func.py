9059c3f0e048db20f0b8445e365888a8640462fe
nick <mgy0822@gmail.com>
2024-01-30 21:24:04 +0000

    fixed a bug

--- madhouse/SVD/SVD_func.py
+++ madhouse/SVD/SVD_func.py
@@ -19,13 +19,10 @@ def SVD_inference(image_path):
     image = load_image(image_path)
     image = image.resize((1024, 576))
     name_without_extension = os.path.splitext(image_path)[0].split('/')[-1]
-    
     for i in range(repeat):
-        # change seed each time repeat
         seed_num=randint(1, 100)
         generator = torch.manual_seed(seed_num)
         
-        # inference
         frames = pipe(
                         image, \
                         decode_chunk_size=decode_chunk_size, \
@@ -37,6 +34,6 @@ def SVD_inference(image_path):
         current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
         save_path="outputs/"+name_without_extension+"_output_"+str(i)+"_"+current_time+".mp4"
         export_to_video(frames, save_path, fps=7)
-        return save_path
+        print(save_path)
 
         
